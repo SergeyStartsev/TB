ae04328f91bae2957e017dfe6bfbb2b89707edd7
SergeyStartsev <startcevs@yandex.ru>
2022-10-20 23:01:53 +0500

    input_data.py

---
+++ input_data.py
@@ -0,0 +1,6 @@
+def input_data():
+    last_name = input("Введите фамилию: ")
+    first_name = input("Введите имя: ")
+    phone_number = input("Введите телефон: ")
+    note = input("Введите примечание: ")
+    return [last_name, first_name, phone_number, note]
