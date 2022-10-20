aea1b46b43dc67a34e559c12cd4dc088946ac5e1
SergeyStartsev <startcevs@yandex.ru>
2022-10-20 22:57:48 +0500

    controller.py

---
+++ controller.py
@@ -0,0 +1,12 @@
+import import_data
+import export_data2
+import print_data
+import search_data
+
+# проложить путь к input_data
+
+print("Что нужно сделать:\n\
+    1 - добавить контакт;\n\
+    2 - посмотреть все контакты;\n\
+    3 - поиск контакта;\n\
+    4 - удаление контакта ")
